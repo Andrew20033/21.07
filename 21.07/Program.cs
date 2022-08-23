5745216a09784d18938d243900fd28c905c9a3db
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-08-23 20:12:05 +0300

    Добавьте файлы проекта.

---
+++ 21.07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace std;
+
+public class Program
+{
+    static void Main()
+    {
+        //Program1();
+        Program2();
+        Console.ReadKey(true);
+    }
+
+    static void Program1()
+    {
+        var testMagazine = new Magazin("Forbs", "2021", "Richest people", "+325398372", "Forbs@gmail.com", 100);
+        testMagazine.Out();
+        var testMagazine2 = new Magazin();
+        testMagazine2.EnterInfo();
+        testMagazine2.Out();
+    }
+
+    public static void Program2()
+    {
+        var testShop = new Shop("Puma", "1971",
+            "Sport produkt", "+868392953", "puma@gmail.com", 100000);
+        testShop.PrintInfo();
+    }
+
+
+
+}
